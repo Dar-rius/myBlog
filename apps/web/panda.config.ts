a293487f08da633958003c7511cf93ca987da452
Dar_rius <mohamedtine17@gmail.com>
2024-02-06 13:40:00 +0000

    add in package.json pandacss

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -6,25 +6,27 @@ const globalCss = defineGlobalStyles({
     color: "white",
   },
 });
+
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx,astro}"],
+  include: [
+    "./src/**/*.{js,jsx,ts,tsx,astro}",
+    "./pages/**/*.{js,jsx,ts,tsx,astro}",
+  ],
 
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
   theme: {
-    extend: {
-      strictPropertyValues: true,
-    },
+    extend: {},
   },
+
   // The output directory for your css system
   outdir: "styled-system",
 
-  // global style
   globalCss,
 });
