a5aa6dd32d3f2af11c25177b4ddbc3fb874d3fa7
Dar_rius <mohamedtine17@gmail.com>
2024-02-22 18:51:35 +0000

    last last commit

--- apps/api/database/migrations/1703854423309_first.ts
+++ apps/api/database/migrations/1703854423309_first.ts
@@ -15,9 +15,9 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName_1, (table) => {
       table.increments('id').unsigned().primary()
       table.integer('author_id').unsigned().references('user.id').onDelete('CASCADE')
-      table.string('title').notNullable()
+      table.string('title', 20).notNullable()
       table.string('label').notNullable()
-      table.string('preface').notNullable()
+      table.string('preface', 200).notNullable()
       table.string('content').notNullable()
       table.date('created_at').notNullable()
       table.date('updated_at').defaultTo(this.now())
--- apps/api/database/seeders/User.ts
+++ apps/api/database/seeders/User.ts
@@ -1,5 +1,6 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import User from '../../app/Models/User'
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class extends BaseSeeder {
   public async run() {
@@ -7,13 +8,13 @@ export default class extends BaseSeeder {
     await User.createMany([
       {
         username: 'Dar_rius',
-        email: 'dar@gmail.com',
-        password: '@20test20@',
+        email: 'mohamedtine077@gmail.com',
+        password: Env.get('PASSWORD'),
       },
       {
-        username: 'Alhamded',
-        email: 'Al@gmail.com',
-        password: '@20test20@',
+        username: 'Dar_rius_',
+        email: 'mohamedtine17@gmail.com',
+        password: Env.get('PASSWORD'),
       },
     ])
   }
