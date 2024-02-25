d3758778ddecdfa82083948756f75eac268863c9
Dar_rius <mohamedtine17@gmail.com>
2024-02-25 16:01:24 +0000

    finish

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: netlify(),
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [
     react({
       experimentalReactChildren: true,
